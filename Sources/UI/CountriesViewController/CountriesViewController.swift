de6ff782afacbb43ed27a09046640d09dddc167c
Student <>
2019-01-16 10:30:16 +0200

    CountriesViewController => parsing and representation Countries API response, rework on CountryViewCell

--- Sources/UI/CountriesViewController/CountriesViewController.swift
+++ Sources/UI/CountriesViewController/CountriesViewController.swift
@@ -8,26 +8,65 @@
 
 import UIKit
 
-class CountriesViewController: UIViewController, UITableViewDataSource, RootViewRepresentable {
+class CountriesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, RootViewRepresentable {
     
     typealias RootView = CountriesView
     
+    var model = Countries() {
+        didSet {
+            DispatchQueue.main.async {
+                self.rootView?.countriesTable?.reloadData()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        URL(string: "https://restcountries.eu/rest/v2/all").do {
+            let parser = NetworkManager<[Country]>()
+            parser.loadData(url: $0)
+            parser.observer {
+                switch $0 {
+                case .notLoaded: return
+                case .didStartLoading: return
+                case .didLoad: self.model = Countries(parser.model!)
+                case .didFailedWithError(let error): print(error)
+                }
+            }
+        }
         
         self.rootView?.countriesTable?.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.model.values.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.rootView?.countriesTable?.dequeueReusableCell(withIdentifier: "cellName")
-                 ?? UITableViewCell(style: .default, reuseIdentifier: "cellName")
+        let cellClass = CountryViewCell.self
+
+        let getCell: () -> CountryViewCell? = {
+            let nib = UINib(cellClass)
+            let cells = nib.instantiate(withOwner: nil, options: nil) as? [CountryViewCell]
+            let cell = cells?.first
+            cell?.fillWithModel(self.model.values[indexPath.row])
+
+            return cell
+        }
+
+        let cell = cast(tableView.dequeueReusableCell(withCellClass: cellClass)) ?? getCell()
+
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
         
-        cell.textLabel?.text = "labelText"
-        
-        return cell
+        let cell = tableView.cellForRow(at: indexPath) as? CountryViewCell
+//        let controller = WeatherViewController()
+//        controller.city = cell?.labelCapital?.text ?? ""
+
+//        self.navigationController?.pushViewController(controller, animated: true)
     }
 }
