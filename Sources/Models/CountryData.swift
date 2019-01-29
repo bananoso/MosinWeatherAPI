0058dc074544f4d01ab93f05ea64025f2bb009d5
Student <>
2019-01-29 16:36:51 +0200

    CountryData.swift => add inherit ObservableObject

--- Sources/Models/CountryData.swift
+++ Sources/Models/CountryData.swift
@@ -8,18 +8,46 @@
 
 import Foundation
 
-class CountryData {
+class CountryData: ObservableObject<CountryData.Event> {
 
-    public var weather: Weather?
-    public let country: Country
+    enum Event {
+        case onSetWeather(Weather)
+        case onSetCountry(Country)
+    }
+    
+    public var weather: Weather? {
+        get { return self.wrappedWeather.value }
+        set { self.wrappedWeather.value = newValue }
+    }
+    
+    public var country: Country {
+        get { return self.wrappedCountry.value }
+        set { self.wrappedCountry.value = newValue }
+    }
+    
+    private var wrappedWeather: Wrapper<Weather?>
+    private var wrappedCountry: Wrapper<Country>
     
     init(country: Country, weather: Weather?) {
-        self.country = country
-        self.weather = weather
+        self.wrappedCountry = Wrapper(country)
+        self.wrappedWeather = Wrapper(weather)
+        
+        super.init()
+        self.subscribe()
     }
     
     convenience init(country: Country) {
         self.init(country: country, weather: nil)
+    }
+    
+    private func subscribe() {
+        self.wrappedWeather.observer {
+            $0.do { self.notify(.onSetWeather($0)) }
+        }
+        
+        self.wrappedCountry.observer {
+            self.notify(.onSetCountry($0))
+        }
     }
 }
 
