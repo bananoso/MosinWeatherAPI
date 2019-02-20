f900f2873d3a8ca35f7372d2d359268611f7353c
Student <>
2019-02-20 17:31:33 +0200

    DataBaseService.swift => Added class

--- Sources/Classes/DataBaseService.swift
+++ Sources/Classes/DataBaseService.swift
@@ -7,3 +7,118 @@
 //
 
 import Foundation
+
+import RealmSwift
+
+public protocol DataBaseServiceType {
+    
+    associatedtype StorageType
+    
+    func read(id: ID, action: @escaping (StorageType) -> ())
+    func readAll(action: @escaping ([StorageType]?) -> ())
+    func write(id: ID, action: @escaping (StorageType) -> ())
+    func update(action: () -> ())
+}
+
+public class DataBaseService<Persistence: Persistable>: DataBaseServiceType
+    where Persistence.Storage: AnyObject
+{
+    
+    // MARK: -
+    // MARK: Subtypes
+    
+    public typealias StorageType = Persistence.Storage
+    
+    // MARK: -
+    // MARK: Properties
+    
+    public let persistence: Persistence
+    
+    private var isInWriteTransaction = false
+    private var isInReadTransaction = false
+    
+    private let lock: NSLocking = NSRecursiveLock()
+    
+    // MARK: -
+    // MARK: Init and Deinit
+    
+    public init(persistence: Persistence) {
+        self.persistence = persistence
+    }
+    
+    // MARK: -
+    // MARK: Public
+    
+    public func read(id: ID, action: @escaping (StorageType) -> ()) {
+        self.performStorageTransaction(
+            excluding: self.isInWriteTransaction,
+            condition: { self.isInReadTransaction = $0 },
+            action: {
+                let storage = self.persistence.read(id: id)
+                
+                self.readStorage(storage)
+                action(storage)
+            }
+        )
+    }
+    
+    public func readAll(action: @escaping ([StorageType]?) -> ()) {
+        self.performStorageTransaction(
+            excluding: self.isInWriteTransaction,
+            condition: { self.isInReadTransaction = $0 },
+            action: {
+                let storages = self.persistence.readAll()
+                
+                storages?.forEach(self.readStorage)
+                action(storages)
+            }
+        )
+    }
+    
+    public func write(id: ID, action: @escaping (StorageType) -> ()) {
+        self.update {
+            let storage = self.persistence.read(id: id)
+            
+            self.writeStorage(storage)
+            self.persistence.write(storage: storage, action: action)
+        }
+    }
+    
+    public func update(action: () -> ()) {
+        self.performStorageTransaction(
+            excluding: self.isInReadTransaction,
+            condition: { self.isInWriteTransaction = $0 },
+            action: action
+        )
+    }
+    
+    // MARK: -
+    // MARK: Open
+
+    open func writeStorage(_ storage: StorageType) {
+        
+    }
+    
+    open func readStorage(_ storage: StorageType) {
+        
+    }
+    
+    // MARK: -
+    // MARK: Private
+    
+    private func performStorageTransaction(
+        excluding: @autoclosure () -> Bool,
+        condition: (Bool) -> (),
+        action: () -> ()
+        ) {
+        self.lock.locked {
+            if excluding() {
+                return
+            }
+            
+            condition(true)
+            action()
+            condition(false)
+        }
+    }
+}
